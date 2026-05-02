38b8ea215f009378491214cedc94f269fc1addac
ali46515 <aliarshad.9474@gmail.com>
2026-05-02 11:18:47 +0500

    Bolean flags

--- basics/variables.py
+++ basics/variables.py
@@ -1,8 +1,22 @@
+# Basic language declarations
 age = 30
 full_name = "Ali Arshad"
 sight = 1.5
 # print(age, full_name, sight)
 
+# String concatenation
 first_name = 'Ali'
 last_name = "Arshad"
-print(first_name + " " + last_name)
+# print(first_name + " " + last_name)
+
+# Boolean flags
+is_person = True
+print(is_person)
+print(not is_person)
+print(True and False)
+print(True and True)
+print(False and False)
+print(True or False)
+print(not 1)
+print(not 0)
+print(1 == True)
