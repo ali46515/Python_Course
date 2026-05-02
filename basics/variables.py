0d06f74cb14ab9029ddd319f20bf5540197c5916
ali46515 <aliarshad.9474@gmail.com>
2026-05-02 11:12:47 +0500

    String concatenation

---
+++ basics/variables.py
@@ -0,0 +1,8 @@
+age = 30
+full_name = "Ali Arshad"
+sight = 1.5
+# print(age, full_name, sight)
+
+first_name = 'Ali'
+last_name = "Arshad"
+print(first_name + " " + last_name)
--- variables.py
+++
@@ -1,4 +0,0 @@
-age = 30
-full_name = "Ali Arshad"
-sight = 1.5
-print(age, full_name, sight)
