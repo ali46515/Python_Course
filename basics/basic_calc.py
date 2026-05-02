e5547298d032e46df772c37b3976f14b96815f81
ali46515 <aliarshad.9474@gmail.com>
2026-05-02 11:40:02 +0500

    Implement basic calculations using arith operators

---
+++ basics/basic_calc.py
@@ -0,0 +1,26 @@
+num1 = float(input("Enter number 1: "))
+num2 = float(input("Enter number 2: "))
+sum = num1 + num2
+mult = num1 * num2
+sub = num1 - num2
+div = num1 / num2
+mod = num1 % num2
+pow = num1 ** num2
+floor_div = num1 // num2
+print(
+    "Sum = " + str(sum) + "\n"
+    "Subtract = "
+    + str(sub)
+    + "\n"
+    + "Multiply = "
+    + str(mult)
+    + "\n"
+    + "Divide = "
+    + str(div)
+    + "\nModulus = "
+    + str(mod)
+    + "\nPower = "
+    + str(pow)
+    + "\nFloor Division = "
+    + str(floor_div)
+)
