d3e7a7d4aa18b751b5d21b6b8f2f6f24cfa94699
ali46515 <aliarshad.9474@gmail.com>
2026-05-02 15:22:01 +0500

    String operations

--- basics/string.py
+++ basics/string.py
@@ -3,3 +3,14 @@ string = "Learning python basics"
 # Uppercase and lowercase
 print(string.upper())
 print(string.lower())
+
+# Find and replcae
+print(string.find('ing'))
+print(string.replace('n ', 'n string '))
+
+# Other operations
+print(string.capitalize())
+print(string.casefold())
+print(string.index('n'))
+print(string.rfind(" "))
+print(string.find(" "))
