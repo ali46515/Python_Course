5caa5820342022b4a96ef5eb5f1f4b50b9d9bc6b
ali46515 <aliarshad.9474@gmail.com>
2026-05-02 19:34:19 +0500

    Basic operations and tuples

--- basics/lists.py
+++ basics/lists.py
@@ -1,6 +1,12 @@
 list = [0, 2, 4, 6]
-list.append("3"*3)
+list.append("3" * 3)
 list.reverse()
 print(list.count(2))
 list.remove(6)
 print(list[0:3])
+
+# Basic operations and tuples
+print(list * 3)
+tuple = (1, 2, 4, 4, 4)
+print(tuple.count(4))
+# tuple[0] = 8 tuples are immutable
