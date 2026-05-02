7b1f547979b988a8ae1882489364d85c7216770a
ali46515 <aliarshad.9474@gmail.com>
2026-05-02 19:24:42 +0500

    Loops and range

---
+++ basics/lists.py
@@ -0,0 +1,6 @@
+list = [0, 2, 4, 6]
+list.append("3"*3)
+list.reverse()
+print(list.count(2))
+list.remove(6)
+print(list[0:3])
---
+++ basics/loops.py
@@ -0,0 +1,15 @@
+print(range(5))
+print(range(5, 10))
+print(range(5, 15, 2))
+
+for i in range(5, 15, 2):
+    print(i)
+
+list = ["Ali", 34, "Boy"]
+for item in list:
+    print(item)
+
+i = 0
+while i <= 15:
+    print(i)
+    i += 1
